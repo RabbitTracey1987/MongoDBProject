d0fc171652a8ad47a380c18570e4ac65cd74ad82
Ji Wang <traceyw@vt.edu>
2018-08-24 10:40:07 +0800

    add nodemon test tool

---
+++ users/test/reading_test.js
@@ -0,0 +1,24 @@
+const assert = require('assert')
+const User = require('../src/user')
+describe("reading users out of the database", ()=>{
+	let joe;
+	beforeEach((done)=>{
+    joe = new User({name:"Joe"});
+    joe.save()
+       .then(()=>done());
+	});
+  it('find all users with a name of joe',(done)=>{
+    User.find({name:'Joe'})
+    	.then((users)=>{
+      	assert(users[0]._id.toString()===joe.id.toString());
+      	done();
+    	});    
+  });
+  it('find a user with a particular id',(done)=>{
+  	User.findOne({_id: joe.id})
+  		.then((user)=>{
+  			assert(user.name === "Joe");
+  			done();
+  		});
+  });
+});
