d76acb48d8c6ee13dc5335959ab892c987941f92
Ji Wang <traceyw@vt.edu>
2018-08-30 12:12:19 +0800

    update mongo query

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -1,10 +1,13 @@
 const assert = require('assert')
 const User = require('../src/user')
 describe("reading users out of the database", ()=>{
-	let joe;
+	let joe,may,alex,zach;
 	beforeEach((done)=>{
     joe = new User({name:"Joe"});
-    joe.save()
+    may = new User({name:"May"});
+    alex = new User({name:"Alex"});
+    zach = new User({name:"Zach"});
+    Promise.all([joe.save(),alex.save(),may.save(),zach.save()])
        .then(()=>done());
 	});
   it('find all users with a name of joe',(done)=>{
@@ -20,5 +23,18 @@ describe("reading users out of the database", ()=>{
   			assert(user.name === "Joe");
   			done();
   		});
+  });
+  it('can skip and limit the result set',(done)=>{
+    //Joe May Alex Zach
+    User.find({})
+        .sort({name: 1})
+        .skip(1)
+        .limit(2)
+        .then((users)=>{
+          assert(users.length===2);
+          assert(users[0].name === "Jay");
+          assert(users[1].name === "May");
+          done();
+        });
   });
 });
